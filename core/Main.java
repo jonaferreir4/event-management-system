781b549e7981787123fe3933d29a8a8440eae93b
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-10-08 23:02:49 -0300

    feat: class Time and Date

--- core/Main.java
+++ core/Main.java
@@ -1,15 +1,21 @@
 package core;
 
 import core.Utils.Date;
+import core.Utils.Time;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Date data = new Date(31, 10, 23);
+            Date data = new Date(28, 3, 2001);
+            Time time = new Time(00, 59);
             System.out.println("Data válida: " + data.toString());
+            System.out.println("Hora válida: " + time.toString());
+
             System.out.println("hello wordl!");
         } catch (IllegalArgumentException e) {
             System.out.println("Data inválida: " + e.getMessage());
+            System.out.println("Hora inválida: " + e.getMessage());
+
         }
     }
 }
--- core/Utils/Date.java
+++ core/Utils/Date.java
@@ -40,7 +40,12 @@ public class Date {
     }
 
     public boolean isValidDate(int day, int month, int year) {
-        if (year < 1 || month < 1 || month > 12 || day < 1 || day > 31) {
+        if (year < 1000 || year > 9999 || month < 1 || month > 12 || day < 1 || day > 31) {
+            return false;
+        }
+
+        String dateStr = String.format("%02d/%02d/%04d", day, month, year);
+        if (!dateStr.matches("\\d{2}/\\d{2}/\\d{4}")) {
             return false;
         }
 
