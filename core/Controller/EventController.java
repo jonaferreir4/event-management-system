acb7815930b27a8b263b515b0965d78c5ad6e58e
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-10-09 23:09:21 -0300

    updates in Controller

---
+++ core/Controller/EventController.java
@@ -0,0 +1,15 @@
+package core.Controller;
+
+import core.Model.Users;
+import core.Model.Events;
+
+
+public class EventController {
+    public void createEvent(String Eventname,String type, String category, String description, String address, String theme) {
+        // Lógica para criar um novo evento
+    }
+
+    public void carryOutRegistration(Users user, Events event) {
+        // Lógica para registrar a inscrição de um usuário em um evento
+    }
+}
--- core/View/Menu.java
+++ core/View/Menu.java
@@ -3,15 +3,22 @@ package core.View;
 import java.util.Scanner;
 
 public class Menu {
+    private Scanner scanner;
+
     public Menu() {
+        scanner = new Scanner(System.in);
     }
 
-    public void MainMenu() {
-        Scanner scanner = new Scanner(System.in);
-
+    public Menu MainMenu() {
         while (true) {
-            System.out.println("Bem vindo ao Events! \nDigite um número de acordo com o que deseja fazer:\n");
-            System.out.println("1 - Criar Evento \n2 - Inscrever-se em um evento \n3 - Sair");
+            System.out.println("Bem vindo ao Events! Escolha uma opção:");
+            System.out.println("1 - Cadastro de Usuário");
+            System.out.println("2 - Eventos");
+            System.out.println("3 - Palestrantes");
+            System.out.println("4 - Certificados");
+            System.out.println("5 - Avaliações e Feedbacks");
+            System.out.println("6 - QR Code");
+            System.out.println("7 - Sair");
 
             String input = scanner.nextLine();
 
@@ -20,12 +27,24 @@ public class Menu {
 
                 switch (choice) {
                     case 1:
-                        EventMenu(); 
+                        menuCadastroUsuario();
                         break;
-                    case 2:
-                        // Implemente a lógica para se inscrever em um evento aqui
-                        break;
-                    case 3:
+                    // case 2:
+                    //     menuEventos();
+                    //     break;
+                    // case 3:
+                    //     menuPalestrantes();
+                    //     break;
+                    // case 4:
+                    //     menuCertificados();
+                    //     break;
+                    // case 5:
+                    //     menuAvaliacoesFeedbacks();
+                    //     break;
+                    // case 6:
+                    //     menuQRCode();
+                    //     break;
+                    case 7:
                         System.out.println("Saindo do programa...");
                         scanner.close();
                         System.exit(0);
@@ -39,15 +58,58 @@ public class Menu {
         }
     }
 
-    public void EventMenu() {
-        Scanner scanner = new Scanner(System.in);
+    private void menuCadastroUsuario() {
+    }
 
-        System.out.println("Insira os seguintes dados para criar um evento: \n");
-        System.out.print("Nome do evento: ");
-        String eventName = scanner.nextLine();
+    private void menuEventos() {
+        while (true) {
+            System.out.println("Menu de Eventos:");
+            System.out.println("1 - Criar Evento");
+            System.out.println("2 - Criação de Atividades");
+            System.out.println("3 - Inscrição no Evento");
+            System.out.println("4 - Confirmação de Inscrição no Evento");
+            System.out.println("5 - Inscrição em Atividades");
+            System.out.println("6 - Confirmação de Inscrição em Atividades");
+            System.out.println("7 - Busca de Eventos");
+            System.out.println("8 - Voltar ao Menu Principal");
 
+            String input = scanner.nextLine();
 
-        System.out.println("Evento criado com sucesso: " + eventName);
+            try {
+                int choice = Integer.parseInt(input);
+
+                switch (choice) {
+                    // case 1:
+                    //     criarEvento();
+                    //     break;
+                    // case 2:
+                    //     criarAtividade();
+                    //     break;
+                    // case 3:
+                    //     inscreverNoEvento();
+                    //     break;
+                    // case 4:
+                    //     confirmarInscricaoEvento();
+                    //     break;
+                    // case 5:
+                    //     inscreverEmAtividade();
+                    //     break;
+                    // case 6:
+                    //     confirmarInscricaoAtividade();
+                    //     break;
+                    // case 7:
+                    //     buscarEventos();
+                    //     break;
+                    case 8:
+                        return;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Digite um número válido.");
+            }
+        }
     }
 
 }
